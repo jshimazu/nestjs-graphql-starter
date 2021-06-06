227a971837c42270383dabf5c7c39bd5a036da22
Junya Shimazu <jshimazu0820@gmail.com>
2021-06-06 15:20:57 +0900

    add users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GraphQLModule } from '@nestjs/graphql';
+import { GqlModuleOptions, GraphQLModule } from '@nestjs/graphql';
+import { UsersModule } from './users/users.module';
 @Module({
-  imports: [GraphQLModule.forRoot({})],
+  imports: [
+    GraphQLModule.forRootAsync({
+      useFactory: () => {
+        const schemaModuleOptions: Partial<GqlModuleOptions> = {};
+        const isDevelopment = process.env.NODE_ENV === 'development';
+        const isTest = process.env.NODE_ENV === 'test';
+        console.log(isDevelopment);
+
+        // If we are in development, we want to generate the schema.graphql
+        if (isDevelopment || isTest || process.env.IS_OFFLINE) {
+          schemaModuleOptions.autoSchemaFile = 'src/schema.gql';
+        } else {
+          // For production, the file should be generated
+          schemaModuleOptions.typePaths = ['dist/*.gql'];
+        }
+
+        return {
+          context: ({ req }) => ({ req }),
+          playground: isDevelopment,
+          introspection: isDevelopment,
+          ...schemaModuleOptions,
+        };
+      },
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
